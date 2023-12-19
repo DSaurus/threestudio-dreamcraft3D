0bbfafdfc6adebb1846cf2e3d42868f4d500247c
DSaurus <2238454358@qq.com>
2023-12-19 19:59:05 +0800

    update version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,17 @@
+import threestudio
+from packaging.version import Version
+
+if hasattr(threestudio, "__version__") and Version(threestudio.__version__) >= Version(
+    "0.2.0"
+):
+    pass
+else:
+    if hasattr(threestudio, "__version__"):
+        print(f"[INFO] threestudio version: {threestudio.__version__}")
+    raise ValueError(
+        "threestudio version must be >= 0.2.0, please update threestudio by pulling the latest version from github"
+    )
+
 from .data import image
 from .guidance import stable_diffusion_bsd_guidance
 from .renderer import nvdiff_rasterizer_ref_mask
