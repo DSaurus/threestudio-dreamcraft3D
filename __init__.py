08e47e68099bca10eda466d6d6acc04909498969
DSaurus <2238454358@qq.com>
2023-12-21 12:26:03 +0800

    fast config

--- __init__.py
+++ __init__.py
@@ -13,6 +13,6 @@ else:
     )
 
 from .data import image
-from .guidance import stable_diffusion_bsd_guidance
+from .guidance import stable_diffusion_bsd_guidance, stable_diffusion_bsd_lora_guidance
 from .renderer import nvdiff_rasterizer_ref_mask
 from .system import dreamcraft3d
